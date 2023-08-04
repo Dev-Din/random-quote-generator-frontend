d8d49b81236e4bfdcca9672bc86ec55db06b7ff8
nurdinhajo <nurudiin222@gmail.com>
2023-08-04 12:21:50 +0300

    Assigned Buttons to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ function App() {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
 
-  }
+
+    <div className="App">
+      <header className="App-header">
+        <h1>Random Quote Generator</h1>
+        <QuoteDisplay quote={quote} author={author} />
+        <Button text="Get Random Quote" onClick={fetchRandomQuote} />
+        <Button text="Create New Quote" onClick={createQuote} />
+        <Button text="Update Quote" onClick={updateQuote} />
+        <Button text="Delete Quote" onClick={deleteQuote} />
+      </header>
+    </div>
+ 
+}
 
 export default App;
