7caee37b064130f4528b49e72697da47da7bcde6
nurdinhajo <nurudiin222@gmail.com>
2023-08-04 12:18:29 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css';
+import QuoteDisplay from './components/QuoteDisplay';
+import Button from './components/Button';
+
+function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  }
+
+export default App;
